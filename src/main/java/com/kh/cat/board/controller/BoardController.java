01ab92aefc1e1bc8c6cabbe49a198325d0087f1b
kpdv91 <kpdv91@gamil.com>
2018-07-02 12:06:13 +0900

    보드 패키지 추가

---
+++ src/main/java/com/kh/cat/board/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.kh.cat.board.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BoardController {
+	
+	private Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+}

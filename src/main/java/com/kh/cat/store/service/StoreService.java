af258cc04084ac2be5106a5b275d852a8179f1b8
kpdv91 <kpdv91@gamil.com>
2018-07-04 15:07:29 +0900

    가게 등록-해쉬태그 추가 삭제(진행중)

--- src/main/java/com/kh/cat/store/service/StoreService.java
+++ src/main/java/com/kh/cat/store/service/StoreService.java
@@ -1,8 +1,27 @@
 package com.kh.cat.store.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class StoreService {
+
+	ArrayList<String> list = new ArrayList<String>();
+	
+	public HashMap<String, Object> hashTagAdd(String[] tagArr, String hTag) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(tagArr.length>=10) {
+			map.put("max", true);
+		}else {
+			map.put("max", false);
+			map.put("hTag", hTag);
+			list.add(hTag);
+			map.put("list", list);
+		}
+		return map;
+	}
 
 }

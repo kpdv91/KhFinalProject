2b418d7667b525b98182891047d1ce5e772d012c
subin846 <subin846@naver.com>
2018-07-10 10:46:27 +0900

    멤버 서비스 수정

--- src/main/java/com/kh/cat/member/controller/MemberController.java
+++ src/main/java/com/kh/cat/member/controller/MemberController.java
@@ -26,18 +26,21 @@ public class MemberController {
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 	@Autowired MemberService memberService;
 	
+	//로그인 폼
 	@RequestMapping(value = "/loginForm", method = RequestMethod.GET)
 	public String loginForm() {
 		logger.info("loginForm 페이지 요청");
 		return "member/loginForm";
 	}
 	
+	//로그인
 	@RequestMapping(value = "/login")
 	public ModelAndView loginConfirmPage (@RequestParam HashMap<String, String> params, HttpSession session) {
 		logger.info("login 요청");
 		return memberService.login(params, session);
 	}
 	
+	//로그아웃
 	@RequestMapping("/logout")
 	public String logout(HttpSession session, HttpServletRequest request ){
 		session = request.getSession(false);		
@@ -45,32 +48,17 @@ public class MemberController {
 			session.invalidate();
 			System.out.println("로그아웃 처리 완료");
 		}
-
 		return "home";
-
 	}
 	
-/*	@RequestMapping(value = "/overlay")
-	public void overlay (HttpServletRequest request, HttpServletResponse response) {
-		logger.info("login 요청");
-		return memberService.overlay(request, response);
-	}*/
-	
-
-	@RequestMapping(value = "/emailChkForm", method = RequestMethod.GET)
-	public String emailChkForm() {
-		logger.info("emailChkForm 페이지 요청");
-		return "member/emailChkForm";
-	}	
-	
-	
-	
+	//회원 가입폼
 	@RequestMapping(value = "/joinForm", method = RequestMethod.GET)
 	public String joinForm() {
 		logger.info("joinForm 페이지 요청");
 		return "member/joinForm";
 	}
 	
+	//회원 가입
 	@RequestMapping(value="join", method=RequestMethod.POST)
 	public ModelAndView join(@RequestParam HashMap<String, String> map) {
 		logger.info("회원 가입 요청");
@@ -79,33 +67,32 @@ public class MemberController {
 		logger.info("name : {}",map.get("userName"));
 		logger.info("email : {}",map.get("userEmail"));
 		logger.info("phone : {}",map.get("userPhone"));
-		logger.info("profile : {}",map.get("profile"));
+		//logger.info("profile : {}",map.get("profile"));
 		//성공 = result.jsp, 실패 = writeForm.jsp
 		return memberService.join(map);
 	}	
-	
-	
-	
-	
-	//보네 - (커뮤니티) 파일 업로드 폼
+		
+	//파일 업로드 폼
 	@RequestMapping(value = "/fileUploadForm")
 	public String fileUploadForm() {
 		logger.info("파일 업로드 페이지 이동");
 		return "member/fileUploadForm";
 	}
 
-	//보네 - (커뮤니티) 파일 업로드
+	//파일 업로드
 	@RequestMapping(value = "/fileUpload")
 	public ModelAndView fileUpload(MultipartFile file, HttpSession session) {
 		logger.info("fileUpload 요청");
 		String root = session.getServletContext().getRealPath("/");
 		return memberService.fileUpload(file,root);
 	}
+	
 	@RequestMapping(value = "/profileimg")
 	public @ResponseBody HashMap<String, Object> profileimg(@RequestParam Map<String,String> params) {
 		logger.info("프로필 사진 불러오기 요청");			
 		return memberService.profileimg(params);
 	}
+	
 	@RequestMapping(value = "/profileunder")
 	public @ResponseBody HashMap<String, Object> profileunder(@RequestParam Map<String,String> params) {
 		logger.info("프로필 사진 불러오기 요청");			
